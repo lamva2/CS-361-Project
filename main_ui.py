951694950b8042accb1f2f6f61afb5de83b819f2
Valerie Lam <lamva@oregonstate.edu>
2025-02-23 21:19:49 -0800

    Edited file

--- main_ui.py
+++ main_ui.py
@@ -48,8 +48,29 @@ def data_analysis():
     print("(1) Bar chart (2) Line graph (3) Scatter plot\n")
     visualization_style = input("Please select your visualization style: ")
     user_input = input("Would you like to run the analysis (1), get help (2), or return to the home page (3): ")
+    if int(user_input) == 1:
+        user_input = input(f"Running data analysis will create your custom visual with variable {x_variable} and variable {y_variable}. Are you sure you would like to continue: ")
+    elif int(user_input) == 2:
+        user_input = help_resources()
     return x_variable, y_variable, visualization_style, user_input
 
+def help_resources():
+    print('-' * terminal_width)
+    print("Help & Resources".center(terminal_width), "\n")
+    print("Common FAQ")
+    print("Q. How do I get started?")
+    print("A. Use the home page to navigate through the different pages. First, look at example visuals to get an idea of what to analyze. Then go to the Data Analysis page to start creating your visuals. If you notice any trends or patterns, write those ideas to your notes page. If you need help at any point, navigate to this page!")
+    print("Q. The graph seems to be missing a lot of points. Did I do something wrong?")
+    print("A. Missing points occur because of incomplete data. This means the variable you selected just doesn't have complete data, which is okay! This opens discussion on why we might be missing data here and you can predict what the data may look like with the value you are given.")
+    print("\n")
+    print("Resources")
+    print("Northeast Big Data Innovation Hub: https://www.youtube.com/channel/UCaHjFSdvLnxWeYski3QjU_g")
+    print("NOAA National Weather Service: https://www.weather.gov/")
+    print("\n")
+    print("Creator Contact: Valerie Lam - lamva@oregonstate.edu", "\n")
+    user_input = input("Would you like to enter the home page (1-yes, 0-no): ")
+    return user_input
+    
 
 terminal_width = os.get_terminal_size().columns
 program_name = "❆ SnowStorm ❆".center(terminal_width)
@@ -63,3 +84,10 @@ if user_input == 1:
     #     # Example visuals
     if home_page_input == 2:
         x_variable, y_variable, visualization_style, user_input = data_analysis()
+    elif home_page_input == 4:
+        help_page_input = int(help_resources())
+        if help_page_input == 1:
+            home_page_input == int(home_page())
+            
+    
+        
